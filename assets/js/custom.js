9b524cd1a466cf6cc0173b863510da266a89bbac
abbasinfoicon <85344249+abbasinfoicon@users.noreply.github.com>
2022-05-17 13:32:34 +0530

    ad

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -45,16 +45,28 @@
         })
 
 
-        $('.main-carousel').owlCarousel({
+        $('.community_carousel').owlCarousel({
             loop: true,
+            margin: 0,
+            autoplay: false,
             nav: true,
-            items: 1,
+            dots: false,
             navText: [
                 "<img src='assets/img/left-icon.png' class='left-icon'>",
                 "<img src='assets/img/right-icon.png' class='right-icon'>"
             ],
-            autoplay: true,
             autoplayHoverPause: true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
         });
 
 
@@ -71,60 +83,7 @@
         });
         // popup-video-section end
 
-        // counter-start
-        $('.counter').counterUp({
-            delay: 10,
-            time: 2000
-        });
-        $('.counter').addClass('animated fadeInDownBig');
-        // counter-end
 
-
-        $('.experience-carousel').owlCarousel({
-            loop: true,
-            nav: true,
-            margin: 30,
-            navText: [
-                "<i class='fa-solid fa-arrow-left'></i>",
-                "<i class='fa-solid fa-arrow-right'></i>"
-            ],
-            autoplay: false,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                768: {
-                    items: 2
-                },
-                1000: {
-                    items: 3
-                }
-            }
-        });
-
-        $('.testimonials-carousel').owlCarousel({
-            loop: true,
-            nav: true,
-            margin: 5,
-            navText: [
-                "<i class='fa-solid fa-angle-left'></i>",
-                "<i class='fa-solid fa-angle-right'></i>"
-            ],
-            autoplay: true,
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                768: {
-                    items: 2
-                },
-                1000: {
-                    items: 3
-                }
-            }
-        });
     });
 
 })(jQuery);
